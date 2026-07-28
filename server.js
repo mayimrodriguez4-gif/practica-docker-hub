86259e9c744353505c0078c813fea853b2eb6934
mayim <mayim.rodriguez4@gmail.com>
2026-07-28 18:16:35 -0400

    Primera versión

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hola Mundo desde Docker 🚀</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
